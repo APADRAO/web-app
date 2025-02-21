8001e9bdc1c334ae86ff3fd14fc7eaef0abfeac4
Alexandre Padrao <alexandre.padrao@dbschenker.com>
2025-02-21 11:36:45 -0300

    correções iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p> 
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dbschenker.com/br-pt"
           target="_blank"
           rel="noopener noreferrer"
         >
