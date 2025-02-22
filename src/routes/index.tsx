3614332ad311894af5d85b0bc2abb9ae4253eea1
Alexandre Padrao <alexandre.padrao@dbschenker.com>
2025-02-22 18:59:28 -0300

    x

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
+import { useAppDrawerContext     } from "../shared/contexts";
 
 export const AppRouts = () => {
-    const {toggleTheme } = useAppThemeContext();
+    const {toggleDrawerOpen } = useAppDrawerContext();
     return (
         <Routes>
 
-            <Route path="/pagina-Inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme }>TESTE</Button>} />
+            <Route path="/pagina-Inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen }>TESTE</Button>} />
             <Route path="*" element={<Navigate to="/pagina-Inicial"/>} />
         </Routes>
     );
